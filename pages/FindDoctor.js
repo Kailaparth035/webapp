69e0f59504e82dc4edacdc25417e5cec1aa40e72
kailaparth035 <kailaparth97370@gmail.com>
2022-12-21 10:23:51 +0530

    Latest commit for Doctore screen.

--- pages/FindDoctor.js
+++ pages/FindDoctor.js
@@ -35,7 +35,6 @@ const FindDoctor = () => {
               height: 40,
               justifyContent: "center",
               alignItems: "center",
-              // backgroundColor: "white",
               alignSelf: "center",
               marginTop: 30,
             }}
@@ -47,13 +46,72 @@ const FindDoctor = () => {
             <div className={styles.search_div}>
               <Image
                 src={require("../assets/Images/Search.png")}
-                // style={{ backgroundColor: "white" }}
                 className={styles.seaech_Image}
               />
             </div>
-            <input placeholder="Search" className={styles.search_inpute} />
+            <input
+              placeholder="Search Doctore"
+              className={styles.search_inpute}
+            />
           </div>
         </div>
+      </div>
+      <div
+        className="row"
+        style={{ backgroundColor: "#E6E6E6", paddingTop: 50 }}
+      >
+        <div className="col-1"></div>
+        <div className="col-5" style={{ marginLeft: 60,marginTop:60 }}>
+          <div className="row">
+            <p className={styles.paragraph_style}>
+              Safety of your data is our top priority.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Image src={require("../assets/Images/Green_Right.png")} />
+              <p className={styles.check_item1}>Multi-level security checks</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: -10,
+              }}
+            >
+              <Image src={require("../assets/Images/Green_Right.png")} />
+              <p className={styles.check_item1}> Multiple data backups</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: -12,
+              }}
+            >
+              <Image src={require("../assets/Images/Green_Right.png")} />
+              <p className={styles.check_item1}>
+                Stringent data privacy policies
+              </p>
+            </div>
+            <button className={styles.read_more_button}>
+              <span className={styles.read_more_text}>Read more </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-4">
+          <Image
+            src={require("../assets/Images/Doctore_Image.png")}
+            style={{ float: "right", marginRight: 60 }}
+          />
+        </div>
+        <div className="col-1"></div>
       </div>
     </>
   );
